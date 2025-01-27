d20f6d40a8808fb246fa9d781acfd124d590a5b7
Bhavya Khatri <115460646+Bhavya022@users.noreply.github.com>
2025-01-27 16:08:44 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.DB_NAME || "imf_gadget_db_xou2",
   process.env.DB_USER || "imf_gadget_db_xou2_user",
-  process.env.DB_PASSWORD || "bhavya@22",
+  process.env.DB_PASSWORD || "q0tcyTZ3GTYZpmTJKTNpTfgA3ezKCq1F",
   {
     host: process.env.DB_HOST || "dpg-cubm20qj1k6c73ed01hg-a",
     port: process.env.DB_PORT || 5432,
